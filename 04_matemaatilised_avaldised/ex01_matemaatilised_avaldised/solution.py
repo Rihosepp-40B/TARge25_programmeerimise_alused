0b94a75eae100a3f4bf70dbc936cadd9107be9a9
Riho Sepp <rihosepp@gmail.com>
2025-11-18 14:12:32 +0200

    Kuni triangle arvutused

--- 04_matemaatilised_avaldised/ex01_matemaatilised_avaldised/solution.py
+++ 04_matemaatilised_avaldised/ex01_matemaatilised_avaldised/solution.py
@@ -1,4 +1,6 @@
 """Math exercises."""
+import math
+from math import remainder
 
 
 def sum_and_difference(num_a: int, num_b: int) -> tuple:
@@ -10,38 +12,40 @@ def sum_and_difference(num_a: int, num_b: int) -> tuple:
 
 def float_division(num_a: int, num_b: int) -> float:
     """Divide given variables num_a and num_b and return the result."""
-    # Write your code here
+    division = num_a / num_b
     return division
 
 
 def integer_division(num_a: int, num_b: int) -> int:
     """Divide given variables num_a and num_b and return the result rounded down."""
-    # Write your code here
+    division = num_a // num_b
     return division
 
 
 def powerful_operations(num_a: int, num_b: int) -> tuple:
     """Return the product of given variables, num_a to the power of num_b and the remainder of division of variables."""
-    # Write your code here
+    multiply_numbers = num_a * num_b
+    power = num_a ** num_b
+    remainder = num_a % num_b
     return multiply_numbers, power, remainder
 
 
 def find_average(num_a: int, num_b: int) -> float:
     """Return the average of given variables."""
-    # Write your code here
+    average = (num_a + num_b) / 2
     return average
 
 
 def area_of_a_circle(radius: float) -> float:
     """Calculate and return the area of a circle."""
-    # Write your code here
-    return circle_area
+    circle_area = radius ** 2 * math.pi
+    return round(circle_area, 2)
 
 
 def area_of_an_equilateral_triangle(side_length: float) -> int:
     """Calculate and return the area of an equilateral triangle."""
-    # Write your code here
-    return triangle_area
+    triangle_area = math.sqrt(3) / 4 * side_length ** 2
+    return round(triangle_area)
 
 
 def calculate_discriminant(a: int, b: int, c: int) -> int:
@@ -65,3 +69,35 @@ if __name__ == '__main__':
     addition_result, difference = sum_and_difference(5, 6)
     assert addition_result == 11
     assert difference == -1
+
+    float_division_result = float_division(10, 10)
+    assert isinstance(float_division_result, float)
+    assert 0.99 < float_division_result < 1.01
+    float_division_result = float_division(10, 2)
+    assert 4.99 < float_division_result < 5.01
+
+    integer_division_result = integer_division(10,10)
+    assert isinstance(integer_division_result, int)
+    assert integer_division_result == 1
+    integer_division_result = integer_division(10,2)
+    assert integer_division_result == 5
+
+    multiplication, power, remainder = powerful_operations(3, 4)
+    assert multiplication == 12
+    assert power == 81
+    assert remainder == 3
+
+    multiplication, power, remainder = powerful_operations(10, 2)
+    assert multiplication == 20
+    assert power == 100
+    assert remainder == 0
+
+    average_result = find_average(3 , 10)
+    assert isinstance(average_result, float)
+    assert 6.49 < average_result < 6.51
+    average_result = find_average(2, 10)
+    assert 5.99 < average_result < 6.01
+
+    area_of_a_circle_result = area_of_a_circle(3)
+    assert 28.269 < area_of_a_circle_result < 28.271
+
