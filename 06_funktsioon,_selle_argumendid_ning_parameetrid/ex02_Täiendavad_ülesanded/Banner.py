11b166aac52dc7494c7fdd2a60a905451c80a7ad
Riho <rihosepp@gmail.com>
2025-12-09 14:12:49 +0200

    Kustutatud asjad

--- 06_funktsioon,_selle_argumendid_ning_parameetrid/ex02_Täiendavad_ülesanded/Banner.py
+++
@@ -1,9 +0,0 @@
-def banner(slogan: str) -> str:
-    return slogan.upper()
-
-
-if __name__ == '__main__':
-    repeat_count = int(input("Mitu korda soovid sloganit korrata? "))
-    slogan = input("Milline on sinu slogan? ")
-    banner_text = banner(slogan)
-    print(f"{banner_text}\n" * repeat_count)
--- 06_funktsioon,_selle_argumendid_ning_parameetrid/ex02_Täiendavad_ülesanded/Elektrihind.py
+++
@@ -1,8 +0,0 @@
-def elektrihind(price_s_kw: float) -> float:
-    price_in_eur = price_s_kw / 100
-    return price_in_eur * 1000
-
-if __name__ == '__main__':
-    given_price_str = input("Sisesta elektrihind sentides kilovatt-tunni kohta: ")
-    converted_price = elektrihind(float(given_price_str))
-    print(f"{given_price_str} s/kWh on {converted_price} €/MWh")
